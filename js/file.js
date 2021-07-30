6067b91aaeb842b26c149f2c7c6843b9926ad75c
8ctopus <hello@octopuslabs.io>
2021-07-30 10:54:33 +0500

    improved: file example

--- js/file.js
+++ js/file.js
@@ -4,40 +4,52 @@ export class file
 {
     static #handle = null;
 
+    /**
+     * Open file
+     * @param path - file path
+     * @return void
+     */
     static open(path)
     {
         try {
+            console.debug("Open file...");
+
             sys.fs.open(path, "w", 0o666)
                 .then(
                     (handle) => {
                         this.#handle = handle;
                     },
                     (error) => {
-                        console.error(`open file - FAILED - ${error}`);
+                        console.error(`Open file - FAILED - ${error}`);
                     });
         }
         catch (e) {
-            console.error(`open file - FAILED - ${e.toString()}`);
+            console.error(`Open file - FAILED - ${e.toString()}`);
         }
     }
 
+    /**
+     *
+     */
     static close()
     {
         try {
-            if (this.#handle)
+            if (this.#handle) {
                 this.#handle.close();
-
-            this.#handle = null;
+                this.#handle = null;
+            }
+            else
+            console.error(`Close file - FAILED - file not open`);
         }
         catch (e) {
-            console.error(`close file - FAILED - ${e.toString()}`);
+            console.error(`Close file - FAILED - ${e.toString()}`);
         }
     }
 
     static write(message)
     {
         if (this.#handle === null) {
-            console.error(`write - FAILED - file not open`);
+            console.error(`Write to file - FAILED - file not open`);
             return;
         }
     }
